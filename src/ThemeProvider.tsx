9ae724e2cfc0d1cd51c1c61ac049ae95231daf79
Mark Kour <mark.kour@icloud.com>
2023-09-15 10:06:55 +0200

    style: add reknes colors

--- src/ThemeProvider.tsx
+++ src/ThemeProvider.tsx
@@ -1,7 +1,21 @@
 import { MantineProvider, MantineThemeOverride } from '@mantine/core';
 
 export const theme: MantineThemeOverride = {
-  // colorScheme: 'dark',
+  colors: {
+    reknes: [
+      '#f5fde7',
+      '#ebf7d5',
+      '#d7efac',
+      '#c1e67f',
+      '#afde5a',
+      '#a3d942',
+      '#9dd733',
+      '#88be25',
+      '#77a81c',
+      '#65920d',
+    ],
+  },
+  primaryColor: 'reknes',
 };
 
 interface ThemeProviderProps {
