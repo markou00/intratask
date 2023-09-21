7bf48ec955cb2547a938d0eefc546844452148b7
Mark Kour <mark.kour@icloud.com>
2023-09-21 09:03:56 +0200

    style: make the header responsive

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,28 +1,50 @@
-import { ActionIcon, Button, Container, Divider, Group, Image } from '@mantine/core';
+import {
+  ActionIcon,
+  Button,
+  Container,
+  Divider,
+  Group,
+  Image,
+  useMantineTheme,
+} from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import { IconLogout, IconPlus } from '@tabler/icons-react';
 
 interface ILayout {
   children: React.ReactNode;
 }
 
-const Layout = ({ children }: ILayout) => (
-  <>
-    <Container p="md" fluid sx={(theme) => ({ boxShadow: theme.shadows.xs })}>
-      <Group position="apart">
-        <Image width="10rem" height="auto" src="/logo.png" />
-        <Group>
-          <Button radius="md" leftIcon={<IconPlus size="1.2rem" />}>
-            Ny Avvik
-          </Button>
-          <Divider orientation="vertical" size="sm" />
-          <ActionIcon variant="transparent" size="lg">
-            <IconLogout />
-          </ActionIcon>
+const Layout = ({ children }: ILayout) => {
+  const theme = useMantineTheme();
+  // TODO: replace this with fn.smallerThan
+  const isMobile = useMediaQuery('(max-width: 30em)');
+
+  return (
+    <>
+      <Container p="md" fluid sx={{ boxShadow: theme.shadows.xs }}>
+        <Group position="apart">
+          <Image width="10rem" height="auto" src="/logo.png" />
+          <Group>
+            {!isMobile && (
+              <Button radius="md" leftIcon={<IconPlus size="1.2rem" />}>
+                Ny Avvik
+              </Button>
+            )}
+            {isMobile && (
+              <ActionIcon radius="md" color="teams" variant="filled" size="lg">
+                <IconPlus size="1.2rem" />
+              </ActionIcon>
+            )}
+            <Divider orientation="vertical" size="sm" />
+            <ActionIcon variant="transparent" size="lg">
+              <IconLogout />
+            </ActionIcon>
+          </Group>
         </Group>
-      </Group>
-    </Container>
-    {children}
-  </>
-);
+      </Container>
+      {children}
+    </>
+  );
+};
 
 export default Layout;
