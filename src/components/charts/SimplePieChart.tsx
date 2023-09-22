77ecdafb5a10f1944ba1ac17929fe3ece9acae41
Mark Kour <mark.kour@icloud.com>
2023-09-22 09:30:35 +0200

    feat: aggregate data by category when no filter is applied

--- src/components/charts/SimplePieChart.tsx
+++ src/components/charts/SimplePieChart.tsx
@@ -1,13 +1,19 @@
 import { Cell, Legend, Pie, PieChart, ResponsiveContainer } from 'recharts';
+import { useFilters } from '../../context/FilterContext';
+import { filterByYear } from '../../utils/filter';
 
 const data = [
-  { name: 'Group A', value: 400 },
-  { name: 'Group B', value: 300 },
-  { name: 'Group C', value: 300 },
-  { name: 'Group D', value: 200 },
+  { year: 2022, name: 'HMS', value: 400 },
+  { year: 2022, name: 'Kvalitet', value: 300 },
+  { year: 2022, name: 'Ytremiljø', value: 300 },
+  { year: 2022, name: 'Intern', value: 200 },
+  { year: 2021, name: 'HMS', value: 400 },
+  { year: 2021, name: 'Kvalitet', value: 300 },
+  { year: 2022, name: 'Ytremiljø', value: 300 },
+  { year: 2023, name: 'Intern', value: 200 },
 ];
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#EE9123'];
 
 const RADIAN = Math.PI / 180;
 const renderCustomizedLabel = ({
@@ -31,26 +37,46 @@ const renderCustomizedLabel = ({
   );
 };
 
-const SimplePieChart = () => (
-  <ResponsiveContainer width="100%" height="100%">
-    <PieChart width={400} height={400}>
-      <Pie
-        data={data}
-        cx="50%"
-        cy="50%"
-        labelLine={false}
-        label={renderCustomizedLabel}
-        outerRadius={80}
-        fill="#8884d8"
-        dataKey="value"
-      >
-        {data.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-        ))}
-      </Pie>
-      <Legend />
-    </PieChart>
-  </ResponsiveContainer>
-);
+const aggregateDataByCategory = (chartData: any[]) =>
+  chartData.reduce((acc, curr) => {
+    const existingEntry = acc.find((item: any) => item.name === curr.name);
+    if (existingEntry) {
+      existingEntry.value += curr.value;
+    } else {
+      acc.push({ ...curr });
+    }
+    return acc;
+  }, [] as any[]);
+
+const SimplePieChart = () => {
+  const { filters } = useFilters();
+
+  let filteredData = filterByYear(filters, data);
+
+  // Check if there's no filter applied based on the length of the filtered data
+  if (filteredData.length === data.length) filteredData = aggregateDataByCategory(filteredData);
+
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <PieChart width={400} height={400}>
+        <Pie
+          data={filteredData}
+          cx="50%"
+          cy="50%"
+          labelLine={false}
+          label={renderCustomizedLabel}
+          outerRadius={80}
+          fill="#8884d8"
+          dataKey="value"
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+          ))}
+        </Pie>
+        <Legend />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+};
 
 export default SimplePieChart;
