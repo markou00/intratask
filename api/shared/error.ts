8c75797a9f10b69f56d1fa12d9ee7f6809d3c4c6
Mark Kour <mark.kour@icloud.com>
2023-10-04 14:30:01 +0200

    feat(api): send generic message when there is a server error

--- api/shared/error.ts
+++ api/shared/error.ts
@@ -1,4 +1,26 @@
-export const handleError = (status: number, message: string) => ({
-  status,
-  body: message,
-});
+export const handleError = (
+  status: number,
+  message: string,
+  logError?: any
+) => {
+  if (logError) {
+    console.error(logError);
+  }
+
+  let responseBody = message;
+
+  if (status >= 500) {
+    responseBody = "Internal Server Error";
+  }
+
+  return {
+    status,
+    body: JSON.stringify({
+      error: true,
+      message: responseBody,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+};
