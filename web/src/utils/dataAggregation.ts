2780f9a1938b2281ac6e89b98e49207b16b9deb3
Mark Kour <mark.kour@icloud.com>
2023-10-05 11:08:05 +0200

    feat(web): add aggregateDeviationsByMonth function

--- web/src/utils/dataAggregation.ts
+++ web/src/utils/dataAggregation.ts
@@ -16,3 +16,59 @@ export const aggregateDataByCategory = (deviations: Deviation[]) =>
     }
     return acc;
   }, [] as { name: string; value: number }[]);
+
+/**
+ * Aggregate the deviations by month and year.
+ *
+ * @param {Array} deviations - The array of deviations to aggregate.
+ * @return {Array} An array with aggregated data in the format { year: number, name: string, avvik: number }
+ */
+export const aggregateDeviationsByMonth = (
+  deviations: {
+    id: number;
+    createdAt: Date;
+    [key: string]: any;
+  }[]
+) => {
+  const monthNames = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  // Transform each deviation into a grouping key: "YYYY-MM"
+  const transformedDeviations = deviations.map((deviation) => {
+    const date = new Date(deviation.createdAt);
+    return {
+      year: date.getFullYear(),
+      month: date.getMonth(),
+      monthName: monthNames[date.getMonth()],
+    };
+  });
+
+  // Use a reducer to aggregate the count by month-year
+  const aggregatedData = transformedDeviations.reduce((acc, curr) => {
+    const key = `${curr.year}-${curr.month}`;
+    if (!acc[key]) {
+      acc[key] = {
+        year: curr.year,
+        name: curr.monthName,
+        avvik: 0,
+      };
+    }
+    acc[key].avvik += 1;
+    return acc;
+  }, {} as Record<string, { year: number; name: string; avvik: number }>);
+
+  // Convert the aggregated data object back into an array
+  return Object.values(aggregatedData);
+};
