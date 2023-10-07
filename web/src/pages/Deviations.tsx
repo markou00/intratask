7c5f6b2770250b8724d667e9ba2b550201e37217
Mark Kour <mark.kour@icloud.com>
2023-10-08 01:03:54 +0200

    feat(web): add more columns

--- web/src/pages/Deviations.tsx
+++ web/src/pages/Deviations.tsx
@@ -1,107 +1,88 @@
-import { Container, MultiSelect, TextInput } from '@mantine/core';
+import {
+  Avatar,
+  Container,
+  Group,
+  MultiSelect,
+  Progress,
+  Text,
+  TextInput,
+  createStyles,
+  rem,
+} from '@mantine/core';
 import { useDebouncedValue } from '@mantine/hooks';
 import { IconSearch } from '@tabler/icons-react';
 import sortBy from 'lodash/sortBy';
 import { DataTable, DataTableSortStatus } from 'mantine-datatable';
 import { useEffect, useMemo, useState } from 'react';
 
+const useStyles = createStyles((theme) => ({
+  progressBar: {
+    '&:not(:first-of-type)': {
+      borderLeft: `${rem(3)} solid ${
+        theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white
+      }`,
+    },
+  },
+}));
+
 const companies = [
   {
-    id: '9d7b6df5-aa1e-4203-bfa8-7d9464e331cb',
-    name: 'Sipes Inc',
-    streetAddress: '280 Rigoberto Divide',
-    city: 'Twin Falls',
-    state: 'MT',
-    missionStatement: 'Strategize magnetic vortals.',
+    id: '1',
+    title: 'Ticket #32734 har flere lignende tickets',
+    date: '2023-08-04',
+    category: 'Kvalitet',
+    creator: 'Rolf Waage', // should be an ID to a user which we can use to fetch name and picture
+    status: 'Ny',
+    assignee: 'Bjørn Inge Molvær', // should be an ID to a user which we can use to fetch name and picture
+    progress: 0,
   },
   {
-    id: '3c147f0b-c63f-4830-8ced-f378aad9efc6',
-    name: 'Runolfsdottir - Cummerata',
-    streetAddress: '102 Konopelski Greens',
-    city: 'Missouri City',
-    state: 'KY',
-    missionStatement: 'Leverage one-to-one methodologies.',
+    id: '2',
+    title: 'Ticket #33723 har flere lignende tickets',
+    date: '2023-09-04',
+    category: 'Ytremiljø',
+    creator: 'Rolf Waage', // should be an ID to a user which we can use to fetch name and picture
+    status: 'Påbegynt',
+    assignee: 'Bjørn Inge Molvær', // should be an ID to a user which we can use to fetch name and picture
+    progress: 25,
   },
   {
-    id: '331992e8-7144-49c4-a8fd-fae9a6921b13',
-    name: 'Johnston LLC',
-    streetAddress: '230 Julie Lake',
-    city: 'Hartford',
-    state: 'KY',
-    missionStatement: 'Transition wireless initiatives.',
-  },
-  {
-    id: 'eb089974-a0ed-4ec2-84a3-4d7bd3935b63',
-    name: 'Crist and Sons',
-    streetAddress: '3387 Blick Turnpike',
-    city: 'Attleboro',
-    state: 'WV',
-    missionStatement: 'Revolutionize out-of-the-box infomediaries.',
-  },
-  {
-    id: 'fc257801-7b32-41ca-a31b-57ae6739415b',
-    name: 'Schmidt and Sons',
-    streetAddress: '286 Leif Lock',
-    city: 'Collierville',
-    state: 'AL',
-    missionStatement: 'Optimize bricks-and-clicks eyeballs.',
-  },
-  {
-    id: 'c942ac73-2c51-4bf1-b4a7-04419acf58c0',
-    name: 'Nicolas Group',
-    streetAddress: '09622 Marcel Place',
-    city: 'Highland',
-    state: 'OR',
-    missionStatement: 'Transition vertical interfaces.',
-  },
-  {
-    id: 'ad36f2d0-b186-4f1e-9a04-57e59715dc8f',
-    name: 'Kub and Sons',
-    streetAddress: '8699 Upton Fords',
-    city: 'East Providence',
-    state: 'IN',
-    missionStatement: 'Drive proactive models.',
-  },
-  {
-    id: 'e4a64ab6-4a9f-4f53-8f9e-dbf761fe9a69',
-    name: 'Jakubowski - Rolfson',
-    streetAddress: "191 O'Connell Greens",
-    city: 'San Rafael',
-    state: 'MA',
-    missionStatement: 'Streamline cutting-edge architectures.',
-  },
-  {
-    id: '996fdd92-a399-4bef-9188-b0458ecee682',
-    name: 'Welch - Tremblay',
-    streetAddress: '31622 Isobel Fall',
-    city: 'Olathe',
-    state: 'AR',
-    missionStatement: 'Deploy wireless solutions.',
-  },
-  {
-    id: 'd0d0f9b1-7bb9-4b1e-967d-3ea81de7dd59',
-    name: 'Mueller, Hodkiewicz and Beahan',
-    streetAddress: '21751 Elisa Village',
-    city: 'Grand Prairie',
-    state: 'WA',
-    missionStatement: 'Facilitate bleeding-edge web-readiness.',
+    id: '3',
+    title: 'Ticket #35847 har flere lignende tickets',
+    date: '2023-10-04',
+    category: 'Interne rutiner',
+    creator: 'Rolf Waage', // should be an ID to a user which we can use to fetch name and picture
+    status: 'Ferdig',
+    assignee: 'Bjørn Inge Molvær', // should be an ID to a user which we can use to fetch name and picture
+    progress: 100,
   },
 ];
 
 const Deviations: React.FC = () => {
-  const [records, setRecords] = useState(sortBy(companies, 'city'));
+  const { classes, theme } = useStyles();
+
+  const [records, setRecords] = useState(sortBy(companies, 'date'));
   const [sortStatus, setSortStatus] = useState<DataTableSortStatus>({
-    columnAccessor: 'city',
+    columnAccessor: 'date',
     direction: 'asc',
   });
 
   const [query, setQuery] = useState('');
   const [debouncedQuery] = useDebouncedValue(query, 200);
 
-  const [selectedStates, setSelectedStates] = useState<string[]>([]);
-  const states = useMemo(() => {
-    const states = new Set(companies.map((e) => e.state));
-    return [...states];
+  const [queryID, setQueryID] = useState('');
+  const [debouncedQueryID] = useDebouncedValue(queryID, 200);
+
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
+  const categories = useMemo(() => {
+    const categories = new Set(companies.map((e) => e.category));
+    return [...categories];
+  }, []);
+
+  const [selectedStatus, setSelectedStatus] = useState<string[]>([]);
+  const statuses = useMemo(() => {
+    const statuses = new Set(companies.map((e) => e.status));
+    return [...statuses];
   }, []);
 
   useEffect(() => {
@@ -111,34 +92,63 @@ const Deviations: React.FC = () => {
 
   useEffect(() => {
     setRecords(
-      companies.filter(({ name, state }) => {
+      companies.filter(({ id, title, category, status }) => {
         if (
           debouncedQuery !== '' &&
-          !`${name}`.toLowerCase().includes(debouncedQuery.trim().toLowerCase())
+          !`${title}`.toLowerCase().includes(debouncedQuery.trim().toLowerCase())
         ) {
           return false;
         }
 
-        if (selectedStates.length && !selectedStates.some((s) => s === state)) {
+        if (
+          debouncedQueryID !== '' &&
+          !`${id}`.toLowerCase().includes(debouncedQueryID.trim().toLowerCase())
+        ) {
+          return false;
+        }
+
+        if (selectedCategories.length && !selectedCategories.some((c) => c === category)) {
+          return false;
+        }
+
+        if (selectedStatus.length && !selectedStatus.some((s) => s === status)) {
           return false;
         }
 
         return true;
       })
     );
-  }, [debouncedQuery, selectedStates]);
+  }, [debouncedQueryID, debouncedQuery, selectedCategories, selectedStatus]);
 
   return (
     <Container fluid p="md">
       <DataTable
         columns={[
           {
-            accessor: 'name',
+            accessor: 'id',
+            textAlignment: 'center',
+            ellipsis: true,
             filter: (
               <TextInput
-                label="Employees"
-                description="Show employees whose names include the specified text"
-                placeholder="Search employees..."
+                label="ID"
+                description="#12345"
+                placeholder="Search id..."
+                icon={<IconSearch size={16} />}
+                value={queryID}
+                onChange={(e: any) => setQueryID(e.currentTarget.value)}
+              />
+            ),
+            filtering: queryID !== '',
+          },
+          {
+            accessor: 'title',
+            ellipsis: true,
+
+            filter: (
+              <TextInput
+                label="Title"
+                description="A bug in the system"
+                placeholder="Search title..."
                 icon={<IconSearch size={16} />}
                 value={query}
                 onChange={(e: any) => setQuery(e.currentTarget.value)}
@@ -146,30 +156,128 @@ const Deviations: React.FC = () => {
             ),
             filtering: query !== '',
           },
-          { accessor: 'streetAddress' },
-          { accessor: 'city', sortable: true },
           {
-            accessor: 'state',
+            accessor: 'date',
+            ellipsis: true,
+
+            sortable: true,
+          },
+          {
+            accessor: 'category',
+            ellipsis: true,
+
             filter: (
               <MultiSelect
-                label="Departments "
-                description="Show all employees working at the selected departments"
-                data={states}
-                value={selectedStates}
+                label="Category"
+                description="Show different categories"
+                data={categories}
+                value={selectedCategories}
                 placeholder="Search departments…"
-                onChange={setSelectedStates}
+                onChange={setSelectedCategories}
                 icon={<IconSearch size={16} />}
                 clearable
                 searchable
               />
             ),
-            filtering: selectedStates.length > 0,
+            filtering: selectedCategories.length > 0,
+          },
+          {
+            accessor: 'creator',
+            ellipsis: true,
+
+            // TODO: using the id in the db, fetch the user's name and avatar
+            render: ({ creator }) => (
+              <Group spacing="sm">
+                <Avatar
+                  size={30}
+                  src="https://images.unsplash.com/photo-1624298357597-fd92dfbec01d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80"
+                  radius={30}
+                />
+                <Text fz="sm" fw={500}>
+                  {creator}
+                </Text>
+              </Group>
+            ),
+
+            // render: () => (
+            //   <Group spacing="sm">
+            //     <Avatar size={30} radius={30} />
+            //     <Avatar size={30} src={graph.avatar} radius={30} />
+            //     <Text fz="sm" fw={500}>
+            //       {graph.name}
+            //     </Text>
+            //   </Group>
+            // ),
+          },
+          {
+            accessor: 'status',
+            ellipsis: true,
+
+            filter: (
+              <MultiSelect
+                label="Status"
+                description="Show different status"
+                data={statuses}
+                value={selectedStatus}
+                placeholder="Search status…"
+                onChange={setSelectedStatus}
+                icon={<IconSearch size={16} />}
+                clearable
+                searchable
+              />
+            ),
+            filtering: selectedCategories.length > 0,
+          },
+          {
+            accessor: 'assignee',
+            ellipsis: true,
+
+            render: ({ assignee }) => (
+              <Group spacing="sm">
+                <Avatar
+                  size={30}
+                  src="https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=250&q=80"
+                  radius={30}
+                />
+                <Text fz="sm" fw={500}>
+                  {assignee}
+                </Text>
+              </Group>
+            ),
+          },
+          {
+            accessor: 'progress',
+            ellipsis: true,
+
+            render: ({ progress }) => (
+              <>
+                <Text fz="xs" c="teal" weight={700}>
+                  {progress}%
+                </Text>
+                <Progress
+                  classNames={{ bar: classes.progressBar }}
+                  sections={[
+                    {
+                      value: progress,
+                      color:
+                        theme.colorScheme === 'dark' ? theme.colors.teal[9] : theme.colors.teal[6],
+                    },
+                    {
+                      value: `${100 - progress}`,
+                      color:
+                        theme.colorScheme === 'dark' ? theme.colors.red[9] : theme.colors.red[6],
+                    },
+                  ]}
+                />
+              </>
+            ),
           },
         ]}
         records={records}
         shadow="xs"
         highlightOnHover
         withBorder
+        withColumnBorders
         borderRadius="md"
         minHeight={records.length === 0 ? '10rem' : ''}
         sortStatus={sortStatus}
