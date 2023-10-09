831672f699b8153d80eb45d51816b8516be0087b
Mark Kour <mark.kour@icloud.com>
2023-10-09 10:51:54 +0200

    fix(web): set an active account after successful login

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,9 +1,26 @@
-import { PublicClientApplication } from '@azure/msal-browser';
+import { EventType, PublicClientApplication } from '@azure/msal-browser';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { msalConfig } from './authConfig';
 
 const msalInstance = new PublicClientApplication(msalConfig);
+
+/**
+ * To set an active account after the user signs in, register an event and listen for LOGIN_SUCCESS and LOGOUT_SUCCESS. For more,
+ * visit: https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/events.md
+ */
+msalInstance.addEventCallback((event: any) => {
+  if (event.eventType === EventType.LOGIN_SUCCESS && event.payload.account) {
+    const account = event.payload.account;
+    msalInstance.setActiveAccount(account);
+  }
+
+  if (event.eventType === EventType.LOGOUT_SUCCESS) {
+    if (msalInstance.getAllAccounts().length > 0) {
+      msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0]);
+    }
+  }
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,9 +4,4 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  // Needed for Mantine DataTable to work without problems
-  optimizeDeps: {
-    include: ['esm-dep > cjs-dep'],
-  },
 });
