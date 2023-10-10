3bd44577ca242d99be60aca0584a96206be1699a
Mark Kour <mark.kour@icloud.com>
2023-10-10 18:50:21 +0200

    feat(web): implement updateDeviation

--- web/src/services/DeviationService.ts
+++ web/src/services/DeviationService.ts
@@ -4,3 +4,10 @@ import Api from '../configs/Api';
 export const getDeviations = async (): Promise<Deviation[]> => {
   return (await Api().get('/deviations')).data;
 };
+
+export const updateDeviation = async (
+  id: number,
+  updateData: Partial<Deviation>
+): Promise<Deviation> => {
+  return (await Api().put(`/deviations/${id}`, updateData)).data;
+};
