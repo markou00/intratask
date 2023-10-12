059871264885a90d4068d067a5fb4e7990d862c9
Mark Kour <mark.kour@icloud.com>
2023-10-12 09:39:20 +0200

    feat(api): implement CreateDeviation

--- web/src/services/DeviationService.ts
+++ web/src/services/DeviationService.ts
@@ -5,6 +5,10 @@ export const getDeviations = async (): Promise<DeviationWithTickets[]> => {
   return (await Api().get('/deviations')).data;
 };
 
+export const createDeviation = async (deviationData: Partial<Deviation>): Promise<Deviation> => {
+  return (await Api().post('/deviations', deviationData)).data;
+};
+
 export const updateDeviation = async (
   id: number,
   updateData: Partial<Deviation>
